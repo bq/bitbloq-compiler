3220e65bfab4e8797a6902375d111394a6bba1df
alvarosBQ <johndoe@example.com>
2016-08-30 15:01:42 +0200

    Deleted unneessary console log

--- index.js
+++ index.js
@@ -28,8 +28,6 @@ app.use(bodyParser.json());
 
 
 app.post('/compile', function(req, res) {
-    console.log('req: ', req);
-    console.log('req.timedout: ', req.timedout);
     console.log("req.body.number is : ", req.body.number);
     if (req.body.code && req.body.board) {
         console.log(utils.checkBoardType(req.body.board));
@@ -63,7 +61,6 @@ app.post('/compile', function(req, res) {
                             });
                         } else {
                             console.log("he completado bien la peticion: ", req.body.number);
-                            console.log("a la hora: ", Date.now());
                             res.send({
                                 hex: hex
                             });
@@ -122,7 +119,7 @@ function compile(code, board, number, done) {
                     });
                     pio.on('close', function(exitCode) {
                         if (exitCode === 0) {
-                            console.log("he completado bien la peticion")
+                            console.log("he completado bien la peticion");
                             fs.readFile(path + '.pioenvs/' + board + '/firmware.hex', 'utf8', function(err, contents) {
                                 hex = contents;
                                 done(null, hex);
