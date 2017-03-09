76658033368e99716939b46d78a571326fcd1e65
Álvaro Sánchez Bravo <alvaro.sanchez@bq.com>
2017-03-09 17:25:57 +0100

    Updated bitbloqlibs

--- pioWS/lib/BitbloqMBot/BitbloqMBot.h
+++ pioWS/lib/BitbloqMBot/BitbloqMBot.h
@@ -1,68 +1,96 @@
+/*
+ * BitbloqMBot.h
+ *
+ * Copyright 2016 Alberto Valero <alberto.valero@bq.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ *
+ */
 
-/******************************************************************************
-* MakeBlock MBot control library for Bitbloq
-* 
-*
-* @author Tomas Calvo - tomas.calvo@bq.com
-* @author Alberto Valero - alberto.valero@bq.com
-*
-******************************************************************************/
 
 #ifndef BITBLOQMBOT_H
 #define BITBLOQMBOT_H
 
-class MeRGBLed;
-class MeBuzzer;
-class MeLightSensor;
-class MeDCMotor;
+#include <BitbloqMCore.h>
+#include <BitbloqUS.h>
+#include <BitbloqDCMotor.h>
 
-class MBot
+// class predeclaration
+
+class BitbloqMBot:public BitbloqMCore
 {
 
-	public:
-		MBot(); //public constructor
-		virtual ~MBot(); //virtual public destructor
+public:
+    BitbloqMBot(int lineFollowerPort=-1, int USPort=-1); // public constructor
+    virtual ~BitbloqMBot(); // virtual public destructor
+    
+    void setLineFollowerPort(int port);
+    void setUSPort(int port);
 
-		void init();
-		
-		/**
-		 * Choose led and set color
-		 * @param led led number
-		 * @param red red component of rgb color
-		 * @param green green component of rgb color
-		 * @param blue blue component of rgb color
-		 */
-		void setLed(int led, int red, int green, int blue);
-		void tone(int note, int beat);
-		/**
-		 * gets distance measured by US sensor
-		 * @param port port number
-		 * @return distance in cm*/
-		int getDistance(int port);
-		/**
-		 * gets wether buttons is pressed or not
-		 * @return button status
-		 */
-		int getButtonStatus();
-		/**
-		 * Gets LDR sensor measure (analog)
-		 * @return LDR reading
-		 */
-		int getLightSensor();
-		/**
-		 * gets IR sensor status (digital)
-		 * @param IR sensor pin
-		 * @return IR sensor status
-		 */
-		int getLineFollower(int port);
-		void move(int direction, int speed);
+    /**
+     * Sets pinmode of sensors and actuators (as in standard Arduino setup)
+     */
+    void setup();
 
-	private:
-		MeRGBLed* boardLeds;
-		MeBuzzer* buzzer;
-		MeLightSensor* lightSensor;
-		MeDCMotor* leftMotor;
-		MeDCMotor* rightMotor;
+    /**
+     * gets distance measured by US sensor
+     * @return distance in cm
+     */
+    int readUSMeasuredDistanceCM() const;
+
+    /**
+     * gets distance measured by US sensor
+     * @return distance in inches
+     */
+    int readUSMeasuredDistanceIN() const;
+
+    /**
+     * gets right IR sensor status (digital)
+     * @return IR sensor status
+     */
+    int readRightLineFollowerSensor() const;
+
+
+    /**
+     * gets left IR sensor status (digital)
+     * @return IR sensor status
+     */
+    int readLeftLineFollowerSensor() const;
+
+    /**
+     * gets IR sensor status (digital)
+     * @return IR sensor status, 0 both, 1 left, 2 right
+     */
+    int readLineFollowerSensor() const;
+
+    void move(int direction, int speed);
+    void setRightMotorSpeed(int speed);
+    void setLeftMotorSpeed(int speed);
+
+
+private:
+    int usTriggerPin; /// set from port using ports array.
+    int usEchoPin; /// set from port using ports array.
+    int rightLineFollowerPin; /// set from port using ports array.
+    int leftLineFollowerPin; /// set from port using ports array.
+
+    BitbloqUltrasound* usSensor;
+    BitbloqDCMotor* rightDCMotor;
+    BitbloqDCMotor* leftDCMotor;
 };
 
 #endif
